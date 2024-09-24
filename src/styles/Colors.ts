49c3c36f3a24c416ba3ba2894ad26d9ec697e53e
hyuni106 <suhyuni106@hanmail.net>
2024-09-24 14:13:36 +0900

    add colors.ts

---
+++ src/styles/Colors.ts
@@ -0,0 +1,23 @@
+const Colors = {
+    white: '#ffffff',
+    black: '#000000',
+    transparent: 'transparent',
+    g1: '#343435',
+    g2: '#424345',
+    g3: '#707177',
+    g4: '#B1B1BB',
+    g5: '#E3E3E3',
+    g6: '#F3F3F3',
+    red: '#EB5757',
+    orange: '#F2994A',
+    yellow: '#F2C94C',
+    green1: '#219653',
+    green2: '#27AE60',
+    green3: '#6FCF97',
+    blue1: '#2D68DB',
+    blue2: '#6E92F0',
+    blue3: '#A5C1F8',
+  } as const;
+  
+  export default Colors;
+  
