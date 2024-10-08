492ec6d4b61d393c8f76faf433a037dd3237c801
hyuni106 <suhyuni106@hanmail.net>
2024-10-08 18:15:22 +0900

    work 컴포넌트 및 style 파일 생성

---
+++ src/components/works/index.tsx
@@ -0,0 +1,7 @@
+import { Section } from './styles';
+
+const Works = () => {
+  return <Section>MY WORKS 🧑‍💻</Section>;
+};
+
+export default Works;
---
+++ src/components/works/styles.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+import { Colors } from 'styles';
+
+export const Section = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background: ${Colors.white};
+  padding: 120px 40px 120px 40px;
+`;
