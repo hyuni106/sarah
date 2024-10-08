1350b1c4cc83840f6bd02037d26f5f7d0a367e52
hyuni106 <suhyuni106@hanmail.net>
2024-10-08 18:42:44 +0900

    swiper npm 추가

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,10 @@ import styled from 'styled-components';
 import Header from './components/Header';
 import Main from 'components/main';
 import AboutMe from 'components/about';
+import Works from 'components/works';
+
+import 'swiper/css';
+import 'swiper/css/pagination';
 
 const Div = styled.div`
   margin: 0;
@@ -16,6 +20,7 @@ function App() {
       <Header />
       <Main />
       <AboutMe />
+      <Works />
     </Div>
   );
 }
