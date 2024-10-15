b288ce144376e1e2a96a5b39fb98ed5a7ee12ea8
hyuni106 <suhyuni106@hanmail.net>
2024-10-15 11:34:33 +0900

    skills 컴포넌트 생성

---
+++ src/components/skills/index.tsx
@@ -0,0 +1,12 @@
+import { TitleText } from 'components/common';
+import { Section } from './styles';
+
+const Skills = () => {
+  return (
+    <Section>
+      <TitleText size="32px">SKILLS</TitleText>
+    </Section>
+  );
+};
+
+export default Skills;
