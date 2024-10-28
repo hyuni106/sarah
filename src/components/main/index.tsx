aa2f3e209beb979b8761321a989b82be34e74862
hyuni106 <suhyuni106@hanmail.net>
2024-10-28 11:25:29 +0900

    main 컴포넌트 i18n 적용

--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -10,18 +10,21 @@ const Main = () => {
   return (
     <Section id="main">
       <ContentWrapper>
-        <TitleText size="22px">{t('main_subtitle_1')}</TitleText>
+        <TitleText size="22px">{t('main_subtitle_hi')}</TitleText>
         <TitleText size="44px" margin="8px 0 0 0">
           {t('main_title_frontend')}
         </TitleText>
         <BodyText size="18px" margin="24px 0 0 0" color={Colors.g3}>
-          안녕하세요! <b>React Native를 활용한 크로스 플랫폼 앱</b> 및 <br />
-          안드로이드 앱을 개발하는 5년 차 엔지니어 사라의 포트폴리오 사이트입니다!
+          {t('main_description')
+            .split('\n')
+            .map((line, idx) => (
+              <div key={idx}>{line}</div>
+            ))}
         </BodyText>
 
         <ButtonContainer>
           <Button backgroundColor={Colors.g1} color={Colors.white} borderColor={Colors.transparent}>
-            알아보기
+            {t('learn')}
           </Button>
 
           <Button
@@ -29,7 +32,7 @@ const Main = () => {
             color={Colors.g1}
             borderColor={Colors.g4}
             margin="0 0 0 16px">
-            Contact
+            {t('contact')}
           </Button>
         </ButtonContainer>
       </ContentWrapper>
