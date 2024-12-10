70fbdf25058bf607b56e6425eeb0ae50d237a236
hyuni106 <suhyuni106@hanmail.net>
2024-12-10 17:27:32 +0900

    server.js 수정

--- server.js
+++ server.js
@@ -1,17 +1,15 @@
-const express = require('express');
-const path = require('path');
+import express from 'express';
+import path from 'path';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Serve static files from the React app's build directory
+const __dirname = path.resolve();
+
 app.use(express.static(path.join(__dirname, 'build')));
 
-// Handle unknown routes by serving index.html
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
